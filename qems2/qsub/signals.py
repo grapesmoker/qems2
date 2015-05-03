b0b76f1a7972bd36d19841e324220f85f1f382d0
mbentley00 <bentley.michael.j@gmail.com>
2015-05-03 13:35:07 -0700

    Import settings into signals

--- qems2/qsub/signals.py
+++ qems2/qsub/signals.py
@@ -9,6 +9,7 @@ from sets import Set
 from django.core.mail import send_mail
 from django.db.models import Q
 import sys
+from django.conf import settings
 
 @receiver(post_save)
 def email_on_comments(sender, instance, created, raw, using, update_fields, **kwargs):
